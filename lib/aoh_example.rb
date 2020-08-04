4854890d61f19800b98774e5ce9e25e802a09ce5
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-04 14:32:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,7 @@ def literal_aoh
   {JOELLE_VD: { name:  "Joelle van Dyne", occupation: "Radio Personality" }),
   {PAT_M: { name:  "Pat Monteseian", occupation: "Staff" }},
   {KATE_G: { name:  "Kate Gompert", occupation: "None" }}, 
-  {BRUCE_G: BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }}
+  {BRUCE_G: { name:  "Bruce Green", occupation: "Fan of Mildred" }}
   ]
 end
 
